83e65f1fc697b275934615d4b8c276190dda08d2
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-11-14 13:51:53 +0530

    Created Custom LinkedList

--- LinkedList/Basics/LinkedList/src/Basics/LL.java
+++ LinkedList/Basics/LinkedList/src/Basics/LL.java
@@ -2,4 +2,25 @@ package Basics;
 
 public class LL {
 
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    private class Node {
+        private int val;
+        private Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
