885673c7ac56377e6300f52886efe8a9a43ed247
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-11-14 21:31:32 +0530

    Insertion at first

--- LinkedList/Basics/LinkedList/src/Basics/LL.java
+++ LinkedList/Basics/LinkedList/src/Basics/LL.java
@@ -23,4 +23,21 @@ public class LL {
             this.next = next;
         }
     }
+
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        head = node;
+
+        // If tail is pointing to null, it means last pointer is pointing to nothing i.e. null,
+        // which means LL is empty, the node which we are inserting is the very first element,
+        // Hence, both, head and tail,has to point these newly added node,
+        // Because, now here we do have only one node in the Linked List
+        if(tail == null) {
+            tail = node;
+        }
+        size += 1;
+    }
 }
---
+++ LinkedList/Basics/LinkedList/src/Basics/Main.java
@@ -0,0 +1,12 @@
+package Basics;
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+        list.insertFirst(50);
+    }
+}
