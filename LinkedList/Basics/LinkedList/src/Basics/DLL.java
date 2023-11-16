236f4eae30d6b6b2c06541fbf28bcbcd321dc8d5
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-11-16 09:13:26 +0530

    Insert at the first position

--- LinkedList/Basics/LinkedList/src/Basics/DLL.java
+++ LinkedList/Basics/LinkedList/src/Basics/DLL.java
@@ -2,6 +2,7 @@ package Basics;
 
 public class DLL {
     private Node head;
+    private int size;
     private class Node {
         int val;
         Node next;
@@ -14,5 +15,26 @@ public class DLL {
             this.next = next;
             this.prev = prev;
         }
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        node.prev = null;
+
+        // If head is pointing to null, then it is obvious, that, prev will also be null, because there is nothing in the DLL
+        // Hence, we have to point the prev pointer of head to the newly created node, only if there is something in th head
+        // Then, only our newly created node will be previous of head or someone
+
+        // If there is nothing in the head, the our newly created node will be previous of what ??? Nothing
+        // Here, in these case, our newly created node is the very first node, so we will direct point
+        // our head to these newly created node
+
+        if(head != null) {
+            head.prev = node;
+        }
+        head = node;
+        size++;
     }
 }
