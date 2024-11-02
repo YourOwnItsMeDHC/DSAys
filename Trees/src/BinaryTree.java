7c63c5c82868be3b60673f8c3429f1fe261e67a3
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2024-11-02 23:01:53 +0530

    Creation of Binary Tree and it's node structure

--- Trees/src/BinaryTree.java
+++ Trees/src/BinaryTree.java
@@ -3,26 +3,44 @@
 public class BinaryTree {
     Node root;
     public BinaryTree() {
-        this.root = null;
     }
-    public BinaryTree(int data) {
-        this.root = new Node(data);
+
+    public void addRoot(int value) {
+        root = new Node(value);
     }
+
+    public void leftChild(Node node, int value) {
+        node.left = new Node(value);
+    }
+
+    public void rightChild(Node node, int value) {
+        node.right = new Node(value);
+    }
+
+    // Display the tree in a landscape ASCII format
+    public void printTree(Node root, String prefix, boolean isLeft) {
+        if (root != null) {
+            System.out.println(prefix + (isLeft ? "├── " : "└── ") + root.value);
+            printTree(root.left, prefix + (isLeft ? "│   " : "    "), true);
+            printTree(root.right, prefix + (isLeft ? "│   " : "    "), false);
+        }
+    }
+
 }
 
 class Node {
-    int data;
-    Node left;
-    Node right;
+    int value;
+    Node left, right;
 
     public Node() {
     }
 
-    public Node(int data) {
-        this.data = data;
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
     }
-    public Node(int data, Node left, Node right) {
-        this.data = data;
+    public Node(int value, Node left, Node right) {
+        this.value = value;
         this.left = left;
         this.right = right;
     }
